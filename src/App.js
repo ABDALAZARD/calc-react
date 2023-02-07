e664f4d74e629dfb5dcb327621b7f18a404d3a87
ABDALAZARD <ABDALAZARD@github.com>
2023-02-07 17:01:17 -0300

    opa

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import Titulo from './components/Title'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Titulo />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='dashboard'>
+          <h1>Dashboard</h1>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/Title/index.js
@@ -0,0 +1,16 @@
+import '../../App.css';
+import logo from '../assets/logo.png';
+
+function Titulo() {
+  return (
+    <div>
+        <div className='navbar'>
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            <h1 className="title">S. C. Corinthians Paulista</h1>
+        </div>
+    </div>
+    
+  );
+}
+
+export default Titulo;

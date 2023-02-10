ca2b4388d23ddce6d9e560636e3df4b897d856d5
ABDALAZARD <ABDALAZARD@github.com>
2023-02-09 23:06:04 -0300

    Cria botao de mais e menos

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import Titulo from './components/Title/index';
 
 function App() {
 
-  const [valor, setValor] = useState(0);
- 
-  
+  const [total, setTotal] = useState(0);
+  // const [mais, setMais] = useState(total);
+  function setMais(value) {
+    return setTotal(total + value);
+  }
+
+  function setMenos(value) {
+    if(total >= 1) {
+      return setTotal( total - value);
+    }
+  }
+
+
   return (
     <div className="container">
       <div className="level">
         <Titulo />
-        <h1 className="panel">{valor}</h1>
+        <h1 className="panel">{total}</h1>
           <div className='buttons'>
-            <button type="button" className="mais" onClick={() => setValor(valor + 1)}>Soma</button>
-            <button type="button" className="menos" onClick={() => setValor(valor - 1)}>Subtrai</button>
+            <button type="button" className="mais" onClick={() => setMais(1)}>Soma</button>
+            <button type="button" className="menos" onClick={() => setMenos(1)}>Subtrai</button>
           </div>
         </div>
     </div>
